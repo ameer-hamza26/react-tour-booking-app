1183aa28d06a3629bf408cd8dc58f0dd77c82c8d
Ameer Hamza <169024920+ameer-hamza26@users.noreply.github.com>
2025-08-17 01:39:04 +0500

    backend completed of this project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline'; // Optional: For consistent global styles
 import App from './App';
 import theme from './globalVariables/Themes'; // Import your custom theme
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
     <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+  </ThemeProvider>
 );
